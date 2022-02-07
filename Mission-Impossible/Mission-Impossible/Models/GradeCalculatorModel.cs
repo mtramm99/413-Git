62841580c6e3acb06c326b459ad389d3cf186ee2
Mark Trammell <mtramm99@gmail.com>
2022-02-07 13:54:13 -0700

    Mission 3 Assignment

---
+++ Mission-Impossible/Mission-Impossible/Models/GradeCalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_Impossible.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0, 100, ErrorMessage ="Enter number between 0 to 100")]
+        public int Assignments { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int Projects { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int Quizzes { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int Exams { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int Intex { get; set; }
+    }
+}
